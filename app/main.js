e45c8398b9cc06925f3c5f88548eec3814193349
Ayansama <ayanforcode@gmail.com>
2024-07-13 21:50:00 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -5,10 +5,16 @@ const net = require("net");
 
 // Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  
    socket.on("close", () => {
      socket.end();
+     socket.close();
    });
  });
+ socket.on("data",(data)=>{
+  const path=data.toString().split('')[1];
+  const responseStatus = path ==='/'? "200 OK":"404 Not Found";
+  socket.write(`HTTP/1.1${responseStatus}\r\n\r\n`);
+ })
 
  server.listen(4221, "localhost");
