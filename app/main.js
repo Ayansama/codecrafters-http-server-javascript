52154aab8646c758bab472a5397c0c0712edb253
Ayansama <ayanforcode@gmail.com>
2024-07-14 00:28:00 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -6,10 +6,13 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
   socket.on("data",(data)=>{
-    const path=data.toString().split('');
-    console.log(path);
-    const responseStatus = (path ==='/')? "200 OK":"404 Not Found";
-    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+    const path=data.toString();
+    if(path.startsWith('GET/')){
+      socket.write("HTTP/1.1 200 OK \r\n\r\n");
+    }
+    else{
+      socket.write("HTTP/1.1 404 NOT FOUND \r\n\r\n");
+    }
    })
   
    socket.on("close", () => {
