94cf057dcc4c3e6f5adbc440d13f56c037a3ba4a
Ayansama <ayanforcode@gmail.com>
2024-07-14 00:30:33 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -8,10 +8,10 @@ console.log("Logs from your program will appear here!");
   socket.on("data",(data)=>{
     const path=data.toString();
     if(path.startsWith('GET/')){
-      socket.write("HTTP/1.1 200 OK \r\n\r\n");
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
     }
     else{
-      socket.write("HTTP/1.1 404 Not Found \r\n\r\n");
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
    })
   
