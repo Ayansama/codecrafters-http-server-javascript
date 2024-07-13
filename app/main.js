4f66a6351534af05356f314291c0d97468d1280e
Ayansama <ayanforcode@gmail.com>
2024-07-14 00:29:36 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -11,7 +11,7 @@ console.log("Logs from your program will appear here!");
       socket.write("HTTP/1.1 200 OK \r\n\r\n");
     }
     else{
-      socket.write("HTTP/1.1 404 NOT FOUND \r\n\r\n");
+      socket.write("HTTP/1.1 404 Not Found \r\n\r\n");
     }
    })
   
