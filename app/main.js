a7e3763703d31dbe9c9b93e9e9591d218cf2231e
Ayansama <ayanforcode@gmail.com>
2024-07-15 23:57:37 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -3,6 +3,14 @@ const fs = require("fs");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+const args = {};
+process.argv.forEach((arg, index) => {
+  if (arg.startsWith("--")) {
+    args[arg.replace(/^--/, "")] = process.argv[index + 1];
+  }
+});
+1;
+const FILES_DIR = args["directory"];
 
 // Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
@@ -25,17 +33,7 @@ console.log("Logs from your program will appear here!");
       console.log(`userAgent:${userAgent}`)
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
     }
-    else if(arr.startsWith('POST')){
-      console.log("in post elif")
-      const content=arr[6].toString();
-      console.log(`content: ${content}`)
-      const directory=process.argv;
-      console.log(directory);
-      const filename=url;
-      fs.writeFileSync(`${directory}/${filename}`,`${content}`,'utf8');
-      socket.write(`HTTP/1.1 201 Created\r\n\r\n`);
-
-    }
+    
     else if(url.startsWith('/files')){
       const fileName=url.split('/files')[1];
       const directory=process.argv[3];
@@ -47,11 +45,24 @@ console.log("Logs from your program will appear here!");
       }
       else{socket.write("HTTP/1.1 404 Not Found\r\n\r\n")};
     }
+    else if(arr.startsWith("/files/")&&
+    data.toString().split(' ')[0]==="POST"){
+      let fileName=url.split('/')[2];
+      console.log(`filename:${fileName}`);
+      let filePath = FILES_DIR+fileName;
+      const file = data.toString("utf-8").split("\r\n\r\n")[1];
+      console.log(`file: ${file}`);
+      fs.writeFileSync(filePath,file);
+      socket.write(`HTTP/1.1 201 Created\r\n\r\n`);
+  
+    }
     
     else{
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
+   
    })
+  
   
    socket.on("close", () => {
      socket.end();
