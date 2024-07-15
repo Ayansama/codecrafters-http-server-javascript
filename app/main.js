8870c75d5b3b93938b1d43ba8672b861be09fffd
Ayansama <ayanforcode@gmail.com>
2024-07-15 17:27:44 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require("fs");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -7,8 +8,9 @@ console.log("Logs from your program will appear here!");
  const server = net.createServer((socket) => {
   socket.on("data",(data)=>{
     let arr=data.toString().split('\r\n');
-    console.log(arr)
+    console.log(`arr:${arr}`)
     const statusLine=arr[0];
+    console.log(`statusLine:${statusLine}`)
     const url=statusLine.split(' ')[1];
   
 
@@ -18,13 +20,23 @@ console.log("Logs from your program will appear here!");
     }
     else if(url.includes('/echo/')){
       const content=url.split('/echo/')[1];
-      console.log(content);
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
     }
     else if(url=="/user-agent"){
       const userAgent=arr[2].split('User-Agent: ')[1];
-      console.log(userAgent)
+      console.log(`userAgent:${userAgent}`)
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+    }
+    else if(statusLine.StartsWith('/files')){
+      const fileName=statusLine.split('/files')[1];
+      const directory=process.argv[3];
+      if (fs.existsSync(`${directory}/${fileName}`)){
+        const content=fs.readFileSync(`${directory}/${fileName}`).toString();
+        console.log(`content:${content}`);
+        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
+        socket.write(res);
+      }
+      else{socket.write("HTTP/1.1 404 Not Found\r\n\r\n")};
     }
     else{
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
