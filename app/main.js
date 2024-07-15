e183d03ecf2337f265bd48bd841620d89e625525
Ayansama <ayanforcode@gmail.com>
2024-07-15 23:34:14 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -38,7 +38,8 @@ console.log("Logs from your program will appear here!");
     }
     else if(arr.startsWith('POST')){
       const content=arr[6].toString();
-      const directory=process.argv[3];
+      const directory=process.argv;
+      console.log(directory);
       const filename=url;
       fs.writeFileSync(`${directory}/${filename}`,`${content}`,'utf8');
       socket.write(`HTTP/1.1 201 Created\r\n\r\n`);
